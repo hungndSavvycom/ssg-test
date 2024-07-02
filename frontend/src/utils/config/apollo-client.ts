bb046a1bced8c93d171dbcb97e60c6cfd51fa0c6
hungndSavvycom <hung.nguyen@savvycomsoftware.com>
2024-07-03 00:34:20 +0700

    update url

--- frontend/src/utils/config/apollo-client.ts
+++ frontend/src/utils/config/apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  uri: 'http://13.251.132.216:4100/graphql',
   cache: new InMemoryCache()
 });
 
